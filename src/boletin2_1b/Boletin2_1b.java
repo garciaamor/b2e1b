c6fe2ab505ca9d426e593770c0a10dab67a82328
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2015-10-07 11:00:43 +0200

    Cambio mensaje

---
+++ src/boletin2_1b/Boletin2_1b.java
@@ -0,0 +1,24 @@
+
+
+package boletin2_1b;
+
+import java.util.Scanner;
+
+public class Boletin2_1b {
+
+ 
+
+   
+     public static void main(String[] args) {
+        float base = 2.0f;//se pone el f para darle valor al float//
+        float altura= (float)3.0;//tambien se puede hacer una operacion de cast en vez de poner la f//
+        System.out.println("area = "+base*altura/2);//para hacerlo directamente, sin generar la variable area//
+          
+    }
+    
+}
+        
+ 
+
+  
+
